d104d358a649157e509f2262370a926d2ea9047d
michlee1337 <michlee1337@gmail.com>
2020-07-08 03:06:46 +0000

    fix json write

--- v1/format/structured_cloud_event_test.cc
+++ v1/format/structured_cloud_event_test.cc
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 namespace cloud_events {
 namespace format {
+using ::io::cloudevents::v1::CloudEvent;
 
 TEST(test, runs) {
     ASSERT_EQ(1,1);
@@ -36,5 +37,33 @@ TEST(StructuredCloudEvent, GetSerialized) {
     StructuredCloudEvent sce3 = StructuredCloudEvent(CloudEventFormat::JSON, "etrfyguhjnmhugyutxrxcjkuygilhojihguyrtesyrstduyfkgl");
     ASSERT_EQ(sce3.GetSerializedCloudEvent(), "etrfyguhjnmhugyutxrxcjkuygilhojihguyrtesyrstduyfkgl");
 }
+
+TEST(JsonMarshaller, serialize) {
+    JsonMarshaller m;
+    CloudEvent ce1;
+    ce1.set_id("1");
+    ce1.set_source("/test");
+    ce1.set_spec_version("1.0");
+    ce1.set_type("test");
+    absl::StatusOr<StructuredCloudEvent> sce1 = m.Serialize(ce1);
+    ASSERT_TRUE(sce1);
+    ASSERT_EQ(sce1.value().GetCloudEventFormat(), CloudEventFormat::JSON);
+    ASSERT_EQ(sce1.value().GetSerializedCloudEvent(),"{\"id\":\"1\",\"source\":\"/test\",\"spec_version\":\"1.0\",\"type\":\"test\"}\n");
+    
+    CloudEvent ce2;
+    ce2.set_id("9999999");
+    ce2.set_source("/test/qwertyuiop");
+    ce2.set_spec_version("2.xxxxx");
+    ce2.set_type("not_a_type");
+    absl::StatusOr<StructuredCloudEvent> sce2 = m.Serialize(ce2);
+    ASSERT_TRUE(sce2);
+    ASSERT_EQ(sce2.value().GetCloudEventFormat(), CloudEventFormat::JSON);
+    ASSERT_EQ(sce2.value().GetSerializedCloudEvent(),"{\"id\":\"9999999\",\"source\":\"/test/qwertyuiop\",\"spec_version\":\"2.xxxxx\",\"type\":\"not_a_type\"}\n");
+
+}
+
+TEST(JsonMarshaller, deserialize) {
+}
+
 } // format
 } // cloud_events
