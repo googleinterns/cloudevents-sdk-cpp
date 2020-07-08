5f975a88aa7f50dda40b8fad93692d6e8d1b4d31
michlee1337 <michlee1337@gmail.com>
2020-07-08 01:13:45 +0000

    return created object instead of pointer

--- v1/format.h
+++ v1/format.h
@@ -27,14 +27,14 @@ class Marshaller {
     private: 
         StructuredCloudEvent CreateStructuredCloudEvent(io::cloudevents::v1::CloudEvent* cloud_event, CloudEventFormat format, std::string serialized_cloud_event);
     public:
-        virtual absl::StatusOr<StructuredCloudEvent*> Serialize(io::cloudevents::v1::CloudEvent cloud_event) = 0;
-        virtual absl::StatusOr<StructuredCloudEvent*> Deserialize(std::string serialized_cloud_event) = 0;
+        virtual absl::StatusOr<StructuredCloudEvent> Serialize(io::cloudevents::v1::CloudEvent cloud_event) = 0;
+        virtual absl::StatusOr<StructuredCloudEvent> Deserialize(std::string serialized_cloud_event) = 0;
 };
 
 // class JsonMarshaller: public Marshaller {
 //     public: 
-//         absl::StatusOr<StructuredCloudEvent*> Serialize(io::cloudevents::v1::CloudEvent cloud_event) override;
-//         absl::StatusOr<StructuredCloudEvent*> Deserialize(StructuredCloudEvent) override;
+//         absl::StatusOr<StructuredCloudEvent> Serialize(io::cloudevents::v1::CloudEvent cloud_event) override;
+//         absl::StatusOr<StructuredCloudEvent> Deserialize(StructuredCloudEvent) override;
 // };
 
 } // structured_cloud_events
