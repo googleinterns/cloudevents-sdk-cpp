1d62bd5043668bb643140909a7a5b4cdc3dc2c2f
michlee1337 <michlee1337@gmail.com>
2020-07-08 01:32:40 +0000

    class definition

--- v1/format/json_marshaller.h
+++ v1/format/json_marshaller.h
@@ -13,13 +13,12 @@
 namespace cloud_events {
 namespace format {
 
-// class JsonMarshaller: public Marshaller {
-//     public: 
-//         absl::StatusOr<StructuredCloudEvent> Serialize(io::cloudevents::v1::CloudEvent cloud_event) override;
-//         absl::StatusOr<StructuredCloudEvent> Deserialize(StructuredCloudEvent) override;
-// };
-
-} // format
-} // cloud_events
+class JsonMarshaller: public Marshaller {
+    private:
+        absl::StatusOr<Json::Value> PrintToJson(io::cloudevents::v1::CloudEvent_CloudEventAttribute attr);
+    public: 
+        absl::StatusOr<StructuredCloudEvent> Serialize(io::cloudevents::v1::CloudEvent cloud_event) override;
+        absl::StatusOr<StructuredCloudEvent> Deserialize(std::string serialized_cloud_event) override;
+};
 
 #endif
