cd58b6fa756a6c30bcdfea7b0abf02dd2c5232ae
michlee1337 <michlee1337@gmail.com>
2020-07-08 01:31:50 +0000

    time util

--- v1/format/json_marshaller.h
+++ v1/format/json_marshaller.h
@@ -8,6 +8,7 @@
 #include <google/protobuf/any.pb.h>
 #include <google/protobuf/message.h>
 #include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/time_util.h>
 
 namespace cloud_events {
 namespace format {
