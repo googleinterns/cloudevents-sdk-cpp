7284bafecf243932746fb99ff5ddc4bb6a7df26d
michlee1337 <michlee1337@gmail.com>
2020-07-08 02:01:14 +0000

    rmv ref to CloudEvent

--- v1/format.cc
+++ v1/format.cc
@@ -6,16 +6,13 @@ using ::io::cloudevents::v1::CloudEvent;
 
 // StructuredCloudEvent
 
-StructuredCloudEvent::StructuredCloudEvent(CloudEvent* cloud_event, CloudEventFormat format, std::string serialized_cloud_event)
-    : format_(format), cloud_event_(cloud_event), serialized_cloud_event_(serialized_cloud_event)
+StructuredCloudEvent::StructuredCloudEvent(CloudEventFormat format, std::string serialized_cloud_event)
+    : format_(format), serialized_cloud_event_(serialized_cloud_event)
 {}
+
 
 CloudEventFormat StructuredCloudEvent::GetCloudEventFormat(){
     return format_;
-}
-
-CloudEvent* StructuredCloudEvent::GetCloudEvent() {
-    return cloud_event_;
 }
 
 std::string StructuredCloudEvent::GetSerializedCloudEvent() {
