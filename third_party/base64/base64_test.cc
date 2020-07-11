ac0c3442cdd1a5af1859e1b27f52ad46c7f0a3dc
michlee1337 <michlee1337@gmail.com>
2020-07-11 01:17:01 +0000

    tests

--- third_party/base64/base64_test.cc
+++ third_party/base64/base64_test.cc
@@ -6,11 +6,11 @@
 TEST(Base64, encode)
 {
    const std::string s =
-       "René Nyffenegger\n"
-       "http://www.renenyffenegger.ch\n"
-       "passion for data\n";
+        "René Nyffenegger\n"
+        "http://www.renenyffenegger.ch\n"
+        "passion for data\n";
 
-   std::string encoded = base64_encode(reinterpret_cast<const unsigned char *>(s.c_str()), s.length());
+   std::string encoded = base64_encode(s);
    std::string decoded = base64_decode(encoded);
 
    ASSERT_EQ(decoded, s);
@@ -27,8 +27,7 @@ TEST(Base64, encode)
    std::string rest0_original = "abc";
    std::string rest0_reference = "YWJj";
 
-   std::string rest0_encoded = base64_encode(reinterpret_cast<const unsigned char *>(rest0_original.c_str()),
-                                             rest0_original.length());
+   std::string rest0_encoded = base64_encode(rest0_original);
    std::string rest0_decoded = base64_decode(rest0_encoded);
 
    ASSERT_EQ(rest0_decoded, rest0_original);
@@ -41,8 +40,7 @@ TEST(Base64, encode)
    std::string rest1_original = "abcd";
    std::string rest1_reference = "YWJjZA==";
 
-   std::string rest1_encoded = base64_encode(reinterpret_cast<const unsigned char *>(rest1_original.c_str()),
-                                             rest1_original.length());
+   std::string rest1_encoded = base64_encode(rest1_original);
    std::string rest1_decoded = base64_decode(rest1_encoded);
 
    ASSERT_EQ(rest1_decoded, rest1_original);
@@ -55,8 +53,7 @@ TEST(Base64, encode)
    std::string rest2_original = "abcde";
    std::string rest2_reference = "YWJjZGU=";
 
-   std::string rest2_encoded = base64_encode(reinterpret_cast<const unsigned char *>(rest2_original.c_str()),
-                                             rest2_original.length());
+   std::string rest2_encoded = base64_encode(rest2_original);
    std::string rest2_decoded = base64_decode(rest2_encoded);
 
    ASSERT_EQ(rest2_decoded, rest2_original);
