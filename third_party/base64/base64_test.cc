0517db67335c33c7f9faa7a91eff17a16fc670d0
michlee1337 <michlee1337@gmail.com>
2020-07-14 20:24:03 +0000

    mit base64

---
+++ third_party/base64/base64_test.cc
@@ -0,0 +1,48 @@
+#include "base64.h"
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+namespace base64 {
+
+TEST(Base64, lib)
+{
+   const std::string s =
+        "René Nyffenegger\n"
+        "http://www.renenyffenegger.ch\n"
+        "passion for data\n";
+
+   std::string encoded = base64_encode(s);
+   std::string decoded = base64_decode(encoded);
+
+   ASSERT_EQ(decoded, s);
+
+   // Test all possibilites of fill bytes (none, one =, two ==)
+   // References calculated with: https://www.base64encode.org/
+
+   std::string rest0_original = "abc";
+   std::string rest0_reference = "YWJj";
+
+   std::string rest0_encoded = base64_encode(rest0_original);
+   std::string rest0_decoded = base64_decode(rest0_encoded);
+
+   ASSERT_EQ(rest0_decoded, rest0_original);
+
+   std::string rest1_original = "abcd";
+   std::string rest1_reference = "YWJjZA==";
+
+   std::string rest1_encoded = base64_encode(rest1_original);
+   std::string rest1_decoded = base64_decode(rest1_encoded);
+
+   ASSERT_EQ(rest1_decoded, rest1_original);
+
+   std::string rest2_original = "abcde";
+   std::string rest2_reference = "YWJjZGU=";
+
+   std::string rest2_encoded = base64_encode(rest2_original);
+   std::string rest2_decoded = base64_decode(rest2_encoded);
+
+   ASSERT_EQ(rest2_decoded, rest2_original);
+}
+
+} // base64
