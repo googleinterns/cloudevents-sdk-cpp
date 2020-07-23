13a8a568ee058fbce158ca4942cec952879db5af
michlee1337 <michlee1337@gmail.com>
2020-07-22 17:20:05 -0700

    struct for internal use

---
+++ v1/event_format/structured_cloud_event.h
@@ -0,0 +1,17 @@
+#ifndef CLOUDEVENTCPPSDK_V1_EVENTFORMAT_STRUCTUREDCLOUDEVENT_H_
+#define CLOUDEVENTCPPSDK_V1_EVENTFORMAT_STRUCTUREDCLOUDEVENT_H_
+
+#include "v1/event_format/format.h"
+
+namespace cloudevents {
+namespace format {
+
+struct StructuredCloudEvent {
+    Format format;
+    std::string name;
+};
+
+} // format
+} // cloudevents
+
+#endif //CLOUDEVENTCPPSDK_V1_EVENTFORMAT_STRUCTUREDCLOUDEVENT_H_
