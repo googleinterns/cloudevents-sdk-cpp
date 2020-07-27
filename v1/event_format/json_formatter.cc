d163eda466ed1bdc42afd882809b851cf6103a0c
michlee1337 <michlee1337@gmail.com>
2020-07-27 23:04:07 +0000

    imports styling

--- v1/event_format/json_formatter.cc
+++ v1/event_format/json_formatter.cc
@@ -1,5 +1,10 @@
 #include "json_formatter.h"
 
+#include <google/protobuf/any.pb.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/time_util.h>
+
 namespace cloudevents {
 namespace format {
 
--- v1/event_format/json_formatter.h
+++ v1/event_format/json_formatter.h
@@ -1,19 +1,12 @@
 #ifndef CLOUDEVENTCPPSDK_V1_EVENTFORMAT_JSONFORMATTER_H_
 #define CLOUDEVENTCPPSDK_V1_EVENTFORMAT_JSONFORMATTER_H_
 
-#include "v1/event_format/formatter.h"
-#include "v1/event_format/structured_cloud_event.h"
-
-#include "proto/cloud_event.pb.h"
-
-#include "third_party/statusor/statusor.h"
-
 #include <json/json.h>
 
-#include <google/protobuf/any.pb.h>
-#include <google/protobuf/message.h>
-#include <google/protobuf/util/json_util.h>
-#include <google/protobuf/util/time_util.h>
+#include "proto/cloud_event.pb.h"
+#include "third_party/statusor/statusor.h"
+#include "v1/event_format/formatter.h"
+#include "v1/event_format/structured_cloud_event.h"
 
 namespace cloudevents {
 namespace format {
