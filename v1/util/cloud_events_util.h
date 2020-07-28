6b71a4d0c43d4b305c9d9ba6eadbd3cc05908754
michlee1337 <michlee1337@gmail.com>
2020-07-28 19:14:13 +0000

    setup

---
+++ v1/util/cloud_events_util.h
@@ -0,0 +1,27 @@
+#ifndef CLOUDEVENTSCPPSDK_V1_UTIL_CLOUDEVENTSUTIL
+#define CLOUDEVENTSCPPSDK_V1_UTIL_CLOUDEVENTSUTIL
+
+#include "third_party/statusor/statusor.h"
+#include "third_party/base64/base64.h"
+#include "proto/cloud_event.pb.h"
+
+namespace cloudevents {
+namespace util {
+
+class CloudEventsUtil {
+    public:
+        // // validate if given CloudEvent fulfills requirements to be valid
+        // static bool IsValid(io::cloudevents::v1::CloudEvent cloud_event);
+
+        // // consolidate metadata in CloudEvent to a single map
+        // static std::map<std::string, io::cloudevents::v1::CloudEvent_CloudEventAttribute> GetMetadata(io::cloudevents::v1::CloudEvent cloud_event);
+        
+        // // convert CloudEvent Attributes to their canonical string representaiton
+        // // as per outlined in https://github.com/cloudevents/spec/blob/master/spec.md#type-system
+        // static absl::StatusOr<std::string> StringifyCeType(io::cloudevents::v1::CloudEvent_CloudEventAttribute attr);    
+};
+
+} // util
+} // cloudevents
+
+#endif //CLOUDEVENTSCPPSDK_V1_UTIL_CLOUDEVENTSUTIL
---
+++ v1/util/cloud_events_util_test.cc
@@ -0,0 +1,12 @@
+#include "cloud_events_util.h"
+#include <gtest/gtest.h>
+
+namespace cloudevents {
+namespace util {
+
+using ::io::cloudevents::v1::CloudEvent;
+using ::io::cloudevents::v1::CloudEvent_CloudEventAttribute;
+
+
+} // util
+} // cloudevents
