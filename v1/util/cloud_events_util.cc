2e3f690f8013a63cbac7a334dabb1ff66f6c38ee
michlee1337 <michlee1337@gmail.com>
2020-07-30 17:01:42 +0000

    setup

---
+++ v1/util/cloud_events_util.cc
@@ -0,0 +1,13 @@
+#include "cloud_events_util.h"
+
+#include <google/protobuf/util/time_util.h>
+
+namespace cloudevents {
+namespace util {
+
+using ::io::cloudevents::v1::CloudEvent;
+using ::io::cloudevents::v1::CloudEvent_CloudEventAttribute;
+
+
+} // util
+} // cloudevents
