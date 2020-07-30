6c9a7b5af4f4a4e9bbeaa8ebd380542dba6760e8
michlee1337 <michlee1337@gmail.com>
2020-07-30 17:07:37 +0000

    StringifyCeType impl

--- v1/util/cloud_events_util.cc
+++ v1/util/cloud_events_util.cc
@@ -41,6 +41,25 @@ absl::StatusOr<std::map<std::string, CloudEvent_CloudEventAttribute>> CloudEvent
     return attrs;
 }
 
+absl::StatusOr<std::string> CloudEventsUtil::StringifyCeType(CloudEvent_CloudEventAttribute attr){
+    switch (attr.attr_oneof_case()) {
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBoolean:
+            return std::string(attr.ce_boolean() ? "true" : "false");  // StatusOr requires explicit conversion
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeInteger:
+            return std::to_string(attr.ce_integer());  // skipping validity checks as protobuf generates int32 for sfixed32
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeString:
+            return attr.ce_string();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBinary:
+            return base64::base64_encode(attr.ce_binary());
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUri:
+            return attr.ce_uri();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUriReference:
+            return attr.ce_uri_reference();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeTimestamp:
+            return google::protobuf::util::TimeUtil::ToString(attr.ce_timestamp());  // take advantage of protobuf using RFC3339 representation
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::ATTR_ONEOF_NOT_SET:
+            return absl::InvalidArgumentError("Cloud Event metadata attribute not set.");
+    }
 }
 
 } // util
--- v1/util/cloud_events_util.h
+++ v1/util/cloud_events_util.h
@@ -18,9 +18,9 @@ class CloudEventsUtil {
             std::map<std::string, io::cloudevents::v1::CloudEvent_CloudEventAttribute>>
         GetMetadata(io::cloudevents::v1::CloudEvent cloud_event);
 
-        // // // convert CloudEvent Attributes to their canonical string representaiton
-        // // // as per outlined in https://github.com/cloudevents/spec/blob/master/spec.md#type-system
-        // static absl::StatusOr<std::string> StringifyCeType(io::cloudevents::v1::CloudEvent_CloudEventAttribute attr);    
+        // convert CloudEvent Attributes to their canonical string representaiton
+        // as per outlined in https://github.com/cloudevents/spec/blob/master/spec.md#type-system
+        static absl::StatusOr<std::string> StringifyCeType(io::cloudevents::v1::CloudEvent_CloudEventAttribute attr);    
 
         // // Constexpr keys used when interacting with CloudEvent
         // inline static constexpr absl::string_view kCeIdKey = "id";
