83cf1e0fbf347c23e8459c3d768ea25522e8d92b
michlee1337 <michlee1337@gmail.com>
2020-08-05 19:42:32 +0000

    setup and header file comments

---
+++ v1/event_format/json_formatter.cc
@@ -0,0 +1,12 @@
+#include "json_formatter.h"
+
+#include <google/protobuf/any.pb.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/time_util.h>
+
+namespace cloudevents {
+namespace format {
+
+} // format
+} // cloudevents
