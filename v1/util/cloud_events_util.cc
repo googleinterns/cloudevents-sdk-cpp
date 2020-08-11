cd30b92cb2615cac9247a7b07c6c5dee76227ed0
michlee1337 <michlee1337@gmail.com>
2020-08-11 18:12:01 +0000

    GetMetadata

--- v1/util/cloud_events_util.cc
+++ v1/util/cloud_events_util.cc
@@ -16,5 +16,32 @@ bool CloudEventsUtil::IsValid(const CloudEvent& cloud_event) {
         cloud_event.type().empty());
 }
 
+absl::StatusOr<
+        absl::flat_hash_map<std::string, CloudEvent_CloudEventAttribute>>
+        CloudEventsUtil::GetMetadata(const CloudEvent& cloud_event) {
+    if (!CloudEventsUtil::IsValid(cloud_event)) {
+        return absl::InvalidArgumentError(
+            "GetMetadata can only be called with valid Cloud Event.");
+    }
+
+    // create absl::flat_hash_map from protobuf map of optional/ extensionattrs
+    absl::flat_hash_map<std::string, CloudEvent_CloudEventAttribute> attrs(
+        (cloud_event.attributes()).begin(),
+        cloud_event.attributes().end());
+
+    // insert required attrs
+    CloudEvent_CloudEventAttribute attr_val;
+    attr_val.set_ce_string(cloud_event.id());
+    attrs["id"] = attr_val;
+    attr_val.set_ce_string(cloud_event.source());
+    attrs["source"] = attr_val;
+    attr_val.set_ce_string(cloud_event.spec_version());
+    attrs["spec_version"] = attr_val;
+    attr_val.set_ce_string(cloud_event.type());
+    attrs["type"] = attr_val;
+
+    return attrs;
+}
+
 }  // namespace cloudevents_util
 }  // namespace cloudevents
--- v1/util/cloud_events_util.h
+++ v1/util/cloud_events_util.h
@@ -14,10 +14,10 @@ class CloudEventsUtil {
         // validate if given CloudEvent fulfills requirements to be valid
         static bool IsValid(const io::cloudevents::v1::CloudEvent& cloud_event);
 
-        // // get metadata from CloudEvent in a single map
-        // static absl::StatusOr<absl::flat_hash_map<
-        //     std::string, io::cloudevents::v1::CloudEvent_CloudEventAttribute>>
-        //     GetMetadata(const io::cloudevents::v1::CloudEvent& cloud_event);
+        // get metadata from CloudEvent in a single map
+        static absl::StatusOr<absl::flat_hash_map<
+            std::string, io::cloudevents::v1::CloudEvent_CloudEventAttribute>>
+            GetMetadata(const io::cloudevents::v1::CloudEvent& cloud_event);
 
         // // TODO (#44): Overload SetMetadata to accept a map of attributes
 
