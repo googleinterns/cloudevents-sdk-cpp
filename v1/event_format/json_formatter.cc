68ef8bb389716860c823bb01a8f9e5c8461eb66d
michlee1337 <michlee1337@gmail.com>
2020-08-15 01:44:43 +0000

    Serialize

--- v1/event_format/json_formatter.cc
+++ v1/event_format/json_formatter.cc
@@ -27,5 +27,50 @@ constexpr char kErrProtobufAny[] = "protobuf::Any not supported yet.";
 constexpr char kErrNotJson[] = "The given serialized data should not be handled by JsonFormatter because it is not JSON-formatted.";
 constexpr char kErrTwoPayloads[] = "The given serialized data is invalid because it contains two data payloads.";
 
+absl::StatusOr<std::unique_ptr<StructuredCloudEvent>> JsonFormatter::Serialize(
+    const CloudEvent& cloud_event) {
+  absl::Status is_valid = CloudEventsUtil::IsValid(cloud_event);
+  if (!is_valid.ok()) {
+    return is_valid;
+  }
+
+  absl::StatusOr<CeAttrMap> attrs = CloudEventsUtil::GetMetadata(
+    cloud_event);
+  if (!attrs.ok()) {
+    return attrs.status();
+  }
+
+  Json::Value root;
+  for (auto const& attr : (*attrs)) {
+    absl::StatusOr<Json::Value> json_printed = PrintToJson(attr.second);
+    if (!json_printed.ok()) {
+      return json_printed.status();
+    }
+    root[attr.first] = (*json_printed);
+  }
+
+  switch (cloud_event.data_oneof_case()) {
+    case CloudEvent::DataOneofCase::kBinaryData:
+      root[kJsonBinaryKey] = cloud_event.binary_data();
+      break;
+    case CloudEvent::DataOneofCase::kTextData:
+      root[kJsonTextKey] = cloud_event.text_data();
+      break;
+    case CloudEvent::DataOneofCase::kProtoData:
+      // TODO (#17): Handle CloudEvent Any in JsonFormatter
+      return absl::UnimplementedError(kErrProtobufAny);
+    case CloudEvent::DATA_ONEOF_NOT_SET:
+      break;
+  }
+
+  // Write JSON serialized data as std::string
+  Json::StreamWriterBuilder builder;
+  auto structured_ce = absl::make_unique<StructuredCloudEvent>();
+  (*structured_ce).format = Format::kJson;
+  (*structured_ce).serialized_data = Json::writeString(builder, root);
+
+  return structured_ce;
+}
+
 }  // namespace format
 }  // namespace cloudevents
--- v1/event_format/json_formatter.h
+++ v1/event_format/json_formatter.h
@@ -16,20 +16,20 @@ namespace format {
  */
 class JsonFormatter: public Formatter {
  public:
-//   // Create Json-formatted serialization from CloudEvent
-//   absl::StatusOr<std::unique_ptr<StructuredCloudEvent>> Serialize(
-//   const io::cloudevents::v1::CloudEvent& cloud_event) override;
+  // Create Json-formatted serialization from CloudEvent
+  absl::StatusOr<std::unique_ptr<StructuredCloudEvent>> Serialize(
+    const io::cloudevents::v1::CloudEvent& cloud_event) override;
 
 //   // Create CloudEvent from Json-formatted serialization
 //   absl::StatusOr<io::cloudevents::v1::CloudEvent> Deserialize(
-//   const StructuredCloudEvent& structured_ce) override;
+//     const StructuredCloudEvent& structured_ce) override;
 
  private:
 //   // Convert from CE to JSON Type System according to
 //   // Relies on the overloaded constructor for Json::Value in jsoncpp
 //   // https://github.com/cloudevents/spec/blob/master/json-format.md#22-type-system-mapping
 //   absl::StatusOr<Json::Value> PrintToJson(
-//   const io::cloudevents::v1::CloudEvent_CloudEventAttribute& attr);
+//     const io::cloudevents::v1::CloudEvent_CloudEventAttribute& attr);
 };
 
 }  // namespace format
