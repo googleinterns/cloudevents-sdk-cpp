b05d19fc2940e9a9ead263af4e8f82ee440106b2
michlee1337 <michlee1337@gmail.com>
2020-08-20 18:44:59 +0000

    setup and tests

---
+++ v1/util/binder_util.cc
@@ -0,0 +1,17 @@
+#include "binder_util.h"
+
+namespace cloudevents {
+namespace binder_util {
+
+constexpr char kMetadataPrefix[] = "ce-";
+constexpr char kContentTypePrefix[] = "application/cloudevents+";
+
+// length of prefix constants are made constexpr to avoid
+// multiple calls to strlen()
+constexpr size_t kMetadataPrefixLen = strlen(kMetadataPrefix);
+constexpr size_t kContentTypePrefixLen = strlen(kContentTypePrefix);
+
+constexpr char kErrNoPrefix[] = "Prefix is not present in the given value.";
+
+}  // namespace binder_util
+}  // namespace cloudevents
