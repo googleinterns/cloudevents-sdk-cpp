44b38cb235e0b6ac245534cdbbb8a24e94c00b58
michlee1337 <michlee1337@gmail.com>
2020-08-25 01:29:07 +0000

    reorganize includes

--- v1/protocol_binding/binder.h
+++ v1/protocol_binding/binder.h
@@ -3,15 +3,12 @@
 
 #include <google/protobuf/message.h>
 
-#include "third_party/statusor/statusor.h"
-#include "third_party/base64/base64.h"
 #include "proto/cloud_event.pb.h"
-#include "external/googleapis/google/pubsub/v1/pubsub.pb.h"
+#include "third_party/statusor/statusor.h"
 #include "v1/event_format/structured_cloud_event.h"
-#include "v1/event_format/json_formatter.h"
-#include "v1/util/formatter_util.h"
-#include "v1/util/cloud_events_util.h"
 #include "v1/util/binder_util.h"
+#include "v1/util/cloud_events_util.h"
+#include "v1/util/formatter_util.h"
 
 namespace cloudevents {
 namespace binding {
