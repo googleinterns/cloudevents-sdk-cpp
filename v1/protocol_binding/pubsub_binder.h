e48840b99ac2b1f48fea99b978989d801d3b42fa
michlee1337 <michlee1337@gmail.com>
2020-08-26 17:56:02 +0000

    rmv seperators in header file

--- v1/protocol_binding/pubsub_binder.h
+++ v1/protocol_binding/pubsub_binder.h
@@ -16,7 +16,6 @@ namespace binding {
 // Uses GoogleApi's PubsubMessage proto.
 class PubsubBinder: public Binder<google::pubsub::v1::PubsubMessage> {
  private:
-  // _____ Operations used in Bind Binary _____
 
   absl::Status BindMetadata(const std::string& key,
     const io::cloudevents::v1::CloudEvent_CloudEventAttribute& val,
@@ -29,15 +28,11 @@ class PubsubBinder: public Binder<google::pubsub::v1::PubsubMessage> {
   absl::Status BindDataText(const std::string& text_data,
     google::pubsub::v1::PubsubMessage& pubsub_msg) override;
 
-  // _____ Operations used in Bind Structured _____
-
   absl::Status BindContentType(const std::string& contenttype,
     google::pubsub::v1::PubsubMessage& pubsub_msg) override;
 
   absl::Status BindDataStructured(const std::string& payload,
     google::pubsub::v1::PubsubMessage& pubsub_msg) override;
-
-  // _____ Operations used in Unbind Binary _____
 
   absl::Status UnbindMetadata(
     const google::pubsub::v1::PubsubMessage& pubsub_msg,
@@ -46,8 +41,6 @@ class PubsubBinder: public Binder<google::pubsub::v1::PubsubMessage> {
   absl::Status UnbindData(
     const google::pubsub::v1::PubsubMessage& pubsub_msg,
     io::cloudevents::v1::CloudEvent& cloud_event) override;
-
-  // _____ Operations used in Unbind Structured _____
 
   cloudevents_absl::StatusOr<std::string> GetContentType(
     const google::pubsub::v1::PubsubMessage& pubsub_msg) override;
